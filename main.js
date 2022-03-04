26d5d38afd358430c541c27c3f775375f3ea35d4
Taylor Cook <taycook91@gmail.com>
2022-03-03 20:57:48 -0600

    added a single round of computer and player decisions as a function

--- main.js
+++ main.js
@@ -3,13 +3,28 @@ let compAnswer = compPlay[Math.floor(Math.random()*compPlay.length)];
 
 
 
-function playRound(playerSelection, computerSelection) {
-    if (computerSelection === playerSelection) {
+function playRound(finalPlayerSelection, computerSelection) {
+    if (computerSelection === finalPlayerSelection) {
         return `You tie, computer chose ${compAnswer}`;
-    } 
+    } else if(computerSelection == 'paper' && finalPlayerSelection == 'rock'){
+        return `you lose, computer chose ${compAnswer}`;
+    } else if(computerSelection == 'rock' && finalPlayerSelection == 'scissors'){
+        return `you lose, computer chose ${compAnswer}`;
+    } else if(computerSelection == 'scissors' && finalPlayerSelection == 'paper'){
+        return `you lose, computer chose ${compAnswer}`;
+    } else if(computerSelection == 'paper' && finalPlayerSelection == 'scissors'){
+        return `you win, ${playerSelection} beats ${compAnswer}`;
+    } else if(computerSelection == 'rock' && finalPlayerSelection == 'paper'){
+        return `you win, ${playerSelection} beats ${compAnswer}`;
+    } else if(computerSelection == 'scissors' && finalPlayerSelection == 'rock'){
+        return `you win, ${playerSelection} beats ${compAnswer}`;
+    } else {
+        return undefined;
+    }
     
 }
 
-let playerSelection = prompt('Lets play Rock, Paper, Scissors! Please type your answer.');
+let playerSelection = prompt("let's play rock, paper, scissors!");
+let finalPlayerSelection = playerSelection.toLowerCase();
 let computerSelection = compAnswer;
 console.log(playRound(playerSelection, computerSelection));
