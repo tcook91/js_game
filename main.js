b11abcee5b97364259b8ace3d8c81a60827d6ffc
Taylor Cook <taycook91@gmail.com>
2022-03-09 17:27:23 -0600

    working conditional chain to account for RPS

--- main.js
+++ main.js
@@ -1,30 +1,56 @@
-let compPlay = ['rock', 'scissors', 'paper'];
-let compAnswer = compPlay[Math.floor(Math.random()*compPlay.length)];
+let playerScore = 0;
+let computerScore= 0;
 
 
 
-function playRound(finalPlayerSelection, computerSelection) {
-    if (computerSelection === finalPlayerSelection) {
-        return `You tie, computer chose ${compAnswer}`;
-    } else if(computerSelection == 'paper' && finalPlayerSelection == 'rock'){
-        return `you lose, computer chose ${compAnswer}`;
-    } else if(computerSelection == 'rock' && finalPlayerSelection == 'scissors'){
-        return `you lose, computer chose ${compAnswer}`;
-    } else if(computerSelection == 'scissors' && finalPlayerSelection == 'paper'){
-        return `you lose, computer chose ${compAnswer}`;
-    } else if(computerSelection == 'paper' && finalPlayerSelection == 'scissors'){
-        return `you win, ${finalPlayerSelection} beats ${compAnswer}`;
-    } else if(computerSelection == 'rock' && finalPlayerSelection == 'paper'){
-        return `you win, ${finalPlayerSelection} beats ${compAnswer}`;
-    } else if(computerSelection == 'scissors' && finalPlayerSelection == 'rock'){
-        return `you win, ${finalPlayerSelection} beats ${compAnswer}`;
+
+//function plays one round of  RPS
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper' || playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "you win!";
+        
+    } else if (playerSelection === computerSelection){
+        return "you tied!";
     } else {
-        return undefined;
+            return "you lose!";
+            
+    }        
+} 
+//plays multiple rounds of RPS up to 5.
+function game(){
+    for(let i=0; i<5; i++) {
+        let compPlay = ['rock', 'scissors', 'paper'];
+        computerSelection = compPlay[Math.floor(Math.random() * compPlay.length)];
+        
+        console.log(computerSelection);
+        playerSelection = prompt("choose rock, paper, or scissors.");
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection))
+        let roundResult = playRound(playerSelection, computerSelection)
+        if(roundResult == "you win!"){
+            playerScore++;
+        } else if (roundResult == "you lose!"){
+            computerScore++;
+        } else {
+            null;
+        }
+    }  
+}
+game();
+console.log(playerScore);
+console.log(computerScore);
+gameEnd(playerScore, computerScore);
+function gameEnd(playerScore, computerScore){
+    if(playerScore === computerScore){
+        return "its a draw!"
+    } else if(playerScore > computerScore){ 
+        return "you win the game!"
+    } else {
+        return "you lose the game!"
     }
-    
 }
 
-let playerSelection = prompt("let's play rock, paper, scissors!");
-let finalPlayerSelection = playerSelection.toLowerCase();
-let computerSelection = compAnswer;
-console.log(playRound(finalPlayerSelection, computerSelection));
+
+
+
+    
